4a84b7e8dbd048deff9777ad4039aea011966b35
Jariar01 <jariar01@estudiantes.unileon.es>
2022-11-09 12:06:59 +0100

    Terminado ejercicio 2 de trabajo de operaciones con un texto por defecto

--- src/com/company/OperacionTexto.java
+++ src/com/company/OperacionTexto.java
@@ -1,30 +1,28 @@
 package com.company;
 
-import java.util.HashMap;
-import java.util.Locale;
-import java.util.Map;
-import java.util.Scanner;
-
+/**
+ * @author Jose Maria
+ * <p>
+ * Clase para realización de operaciones con textos
+ */
 public class OperacionTexto {
-    /**
-     * Metodo principal que llama al resto de metodos
-     */
-    public static void main() {
-        Texto texto = new Texto();
-        texto.cuentaCaracteres();
-        texto.aMayus();
-        texto.aMinus();
-        texto.reemplazo();
-        texto.concatenar();
 
+    public static void main() {
+        operaciones();
     }
 
-
-
-
-
-
-
+    /**
+     * Método donde se indicará las operaciones a realizar con el texto
+     */
+    private static void operaciones() {
+        Texto texto = new Texto();
+        System.out.println("El número total de caracteres de la cadena es: " + texto.cuentaCaracteres());
+        System.out.println("La cadena en mayúsculas queda de la forma: " + texto.aMayus());
+        System.out.println("La cadena en minúsculas queda de la forma: " + texto.aMinus());
+        System.out.println("Palabras repetidas y su número de repeticiones: " + texto.repeticiones());
+        System.out.println("Reemplazamos la palabra 'Proconsi' por 'Isnocorp': " + texto.reemplazo());
+        System.out.println("Tiempo que se tardó en concatenar el texto 1000 veces: " + texto.concatenar() + " ms");
+    }
 
 
 }
--- src/com/company/Texto.java
+++ src/com/company/Texto.java
@@ -1,9 +1,21 @@
 package com.company;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
 
+/**
+ * @author Jose Maria
+ * <p>
+ * Objeto texto con sus respectivas operaciones
+ */
 public class Texto {
+
+    /**
+     * Texto de ejemplo
+     */
     private final String TEXTEJEMPLO = "Proconsi es una empresa de Tecnologías de la Información y la Comunicación especializada en el " +
             "desarrollo e integración de soluciones informáticas para todo tipo de empresas. Más de tres décadas de experiencia " +
             "avalan a una compañía tan flexible como responsable. Cuenta con un equipo multidisciplinar de más de 120 " +
@@ -13,45 +25,54 @@ public class Texto {
             "de tendencias en nuevas tecnologías, como el cloud computing.";
     private String cadena = "";
 
+    /**
+     * Constructor por defecto de la clase.
+     * En caso de que no se pase ninguna cadena por parámetro se usará un texto de ejemplo
+     */
     public Texto() {
         cadena = TEXTEJEMPLO;
     }
 
+    /**
+     * @param cadena La cadena que usaremos como texto con el que trabajar
+     *               <p>
+     *               Constructor para una cadena pasada por parámetro
+     */
     public Texto(String cadena) {
         this.cadena = cadena;
     }
 
     /**
-     * Método encargado de contar el número total de caracteres de la cadena
+     * @return El número de caracteres que tiene la cadena
      */
-    public void cuentaCaracteres() {
-        int caracteres;
+    public BigInteger cuentaCaracteres() {
+        BigInteger caracteres = new BigInteger("0");
 
-        caracteres = cadena.replace(" ", "").length(); //Con esto omitimos que cuenta los espacios
-        System.out.println("Número de caracteres totales: " + caracteres);
+        caracteres.add(BigInteger.valueOf(cadena.replace(" ", "").length())); //Con esto omitimos que cuenta los espacios
+
+        return caracteres;
     }
 
     /**
-     * Método que escribe toda la cadena en mayúsculas
+     * @return La cadena con todos los caracteres en mayúsculas
      */
-    public void aMayus() {
-        String mayúsculas = cadena.toUpperCase();
-        System.out.println("Texto en mayúsculas: " + mayúsculas);
+    public String aMayus() {
+        return cadena.toUpperCase(Locale.ROOT);
     }
 
     /**
-     * Método que escribe toda la cadena en minúsculas
+     * @return La cadena con todos los caracteres en minúsculas
      */
-    public void aMinus() {
-        String minúsculas = cadena.toLowerCase();
-        System.out.println("Texto en minúsculas: " + minúsculas);
+    public String aMinus() {
+        return cadena.toLowerCase();
     }
 
     /**
-     * Método que muestra las palabras que aparecen más de una vez en la cadena
+     * @return El número de palabras repetidas con el número de repeteciones en el texto
      */
-    public void repeticiones() {
+    public String repeticiones() {
         String[] palabras = cadena.split(" ");
+        StringBuilder formato = new StringBuilder();
         HashMap<String, Integer> mapaPalabras = new HashMap<>();
 
         //Primero almacenamos todas las palabras de la cadena en un mapa, donde si se encuentra una palabra por primera vez se añade y si no al valor con el que se relaciona se le sumará 1
@@ -66,38 +87,36 @@ public class Texto {
         //Imprimimos por pantalla todas las palabras con valor > 1
         for (Map.Entry entry : mapaPalabras.entrySet()) {
             if ((Integer) entry.getValue() > 1) {
-                System.out.print(entry.getKey() + "[" + entry.getValue() + "]");
+                formato.append(entry.getKey() + "[" + entry.getValue() + "]");
             }
         }
 
-        System.out.println();
+        return formato.toString();
     }
 
     /**
-     * Método que reemplaza 'Proconsi' por 'Isnocorp'
+     * @return La cadena con la palabra 'Proconsi' sustituida por su inverso
      */
-    public void reemplazo() {
-        String textoRemplazo = cadena.replace("Proconsi", "Isnocorp");
-        System.out.println("Sustituimos 'Proconsi' por 'Isnocorp': " + textoRemplazo);
+    public String reemplazo() {
+        return cadena.replace("Proconsi", "Isnocorp");
     }
 
     /**
-     * Método que concatena el texto 1000 veces y calcula el tiempo que le ha llevado realizar dicho proceso
+     * @return El tiempo en milisegundos que se tardó en concatener el texto 1000 veces
      */
-    public void concatenar() {
-        String concatenacion = "";
+    public BigDecimal concatenar() {
+        StringBuilder concatenacion = new StringBuilder();
 
         //Establecemos un momento de comienzo de operación que luego restaremos al momento final después de realizar la operación para sacar el tiempo total del proceso
-        long inicio = System.currentTimeMillis();
+        BigDecimal inicio = new BigDecimal(System.currentTimeMillis());
 
         for (int i = 0; i < 1000; i++) {
-            concatenacion.concat(cadena);
+            concatenacion.append(cadena);
         }
 
-        long fin = System.currentTimeMillis();
+        BigDecimal fin = new BigDecimal(System.currentTimeMillis());
 
-        long tiempoTotal = fin - inicio;
+        return fin.subtract(inicio);
 
-        System.out.println("Tiempo total en concatener 1000 el texto: " + tiempoTotal + " ms");
     }
 }
